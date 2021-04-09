81e625d30b2cc5d421f1e25e693072071c44eed5
Rafael Vescovi <ravescovi@gmail.com>
2021-04-09 16:19:17 -0500

    new name

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+
+install_requires = []
+with open('requirements.txt') as reqs:
+    for line in reqs.readlines():
+        req = line.strip()
+        if not req or req.startswith('#'):
+            continue
+        install_requires.append(req)
+
+setup(
+    name='gladier_kanzus',
+    description='The Kanzus Gladier',
+    url='https://github.com/globus-gladier/gladier_kanzus',
+    maintainer='The Gladier Team',
+    maintainer_email='',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    dependency_links=[],
+    license='Apache 2.0',
+    classifiers=[]
+)
