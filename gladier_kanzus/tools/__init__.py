133c74f818277085275010120ecc17772db19152
Rafael Vescovi <ravescovi@gmail.com>
2021-04-27 13:08:48 -0500

    sync nick

--- gladier_kanzus/tools/__init__.py
+++ gladier_kanzus/tools/__init__.py
@@ -2,6 +2,9 @@ from gladier.defaults import GladierDefaults as GladierBaseTool
 
 from .create_phil import funcx_create_phil
 from .dials_stills import funcx_stills_process
+from .plot import ssx_plot
+from .publish import ssx_publish
+from .gather_data import ssx_gather_data
 
 __all__ = ['CreatePhil','DialsStills']
 
@@ -22,6 +25,29 @@ class DialsStills(GladierBaseTool):
         funcx_stills_process
     ]
 
+class SSXGatherData(GladierBaseTool):
+    flow_definition = None
+    required_input = []
+    # funcx_endpoints = dict()
+    funcx_functions = [
+        ssx_gather_data
+    ]
+    
+class SSXPlot(GladierBaseTool):
+    flow_definition = None
+    required_input = []
+    # funcx_endpoints = dict()
+    funcx_functions = [
+        ssx_plot
+    ]
+
+class SSXPublish(GladierBaseTool):
+    flow_definition = None
+    required_input = []
+    # funcx_endpoints = dict()
+    funcx_functions = [
+        ssx_publish
+    ]
 
 # DialsVersion = GladierBaseTool()
 
