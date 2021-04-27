c7ab5e477fb006ffb3735713289265486724b6f1
Rafael Vescovi <ravescovi@gmail.com>
2021-04-27 13:58:07 -0500

    sync nick PR#4

--- gladier_kanzus/tools/__init__.py
+++ gladier_kanzus/tools/__init__.py
@@ -1,3 +1,5 @@
+import datetime
+
 from gladier.defaults import GladierDefaults as GladierBaseTool
 
 from .create_phil import funcx_create_phil
@@ -6,7 +8,7 @@ from .plot import ssx_plot
 from .publish import ssx_publish
 from .gather_data import ssx_gather_data
 
-__all__ = ['CreatePhil','DialsStills']
+__all__ = ['CreatePhil','DialsStills','SSXGatherData','SSXPlot','SSXPublish']
 
 
 class CreatePhil(GladierBaseTool):
@@ -32,7 +34,7 @@ class SSXGatherData(GladierBaseTool):
     funcx_functions = [
         ssx_gather_data
     ]
-    
+
 class SSXPlot(GladierBaseTool):
     flow_definition = None
     required_input = []
@@ -49,6 +51,121 @@ class SSXPublish(GladierBaseTool):
         ssx_publish
     ]
 
+
+class SSXGatherData(GladierDefaults):
+
+    flow_definition = {
+      'Comment': 'Gather port-dials data for plot generation and upload',
+      'StartAt': 'SSXPlot',
+      'States': {
+        'SSXPlot': {
+          'Comment': 'Gather port-dials data for plot generation and upload',
+          'Type': 'Action',
+          'ActionUrl': 'https://api.funcx.org/automate',
+          'ActionScope': 'https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/automate2',
+          'ExceptionOnActionFailure': False,
+          'Parameters': {
+              'tasks': [{
+                'endpoint.$': '$.input.funcx_endpoint_non_compute',
+                'func.$': '$.input.ssx_gather_data_funcx_id',
+                'payload.$': '$.input',
+              }]
+          },
+          'ResultPath': '$.SSXGatherData',
+          'WaitTime': 600,
+          'End': True
+        }
+      }
+    }
+
+    flow_input = {
+        'trigger_name': '/projects/APSDataAnalysis/nick/SSX/S9/nsp10nsp16/K/Kaleidoscope_processing',
+        'metadata': {
+            "description": "Automated data processing.",
+            "creators": [{"creatorName": "Kanzus"}],
+            "publisher": "Automate",
+            "subjects": [{"subject": "SSX"}],
+            "publicationYear": str(datetime.datetime.now().year),
+        }
+    }
+
+    required_input = [
+        'trigger_name',
+    ]
+
+    funcx_functions = [
+        ssx_gather_data
+    ]
+
+
+class SSXPlot(GladierDefaults):
+
+    flow_definition = {
+      'Comment': 'Plot SSX data',
+      'StartAt': 'SSXPlot',
+      'States': {
+        'SSXPlot': {
+          'Comment': 'Upload to petreldata, ingest to SSX search index',
+          'Type': 'Action',
+          'ActionUrl': 'https://api.funcx.org/automate',
+          'ActionScope': 'https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/automate2',
+          'ExceptionOnActionFailure': False,
+          'Parameters': {
+              'tasks': [{
+                'endpoint.$': '$.input.funcx_endpoint_non_compute',
+                'func.$': '$.input.ssx_plot_funcx_id',
+                'payload.$': '$.input',
+            }]
+          },
+          'ResultPath': '$.SSXPlot',
+          'WaitTime': 600,
+          'End': True
+        }
+      }
+    }
+
+    flow_input = {}
+
+    required_input = []
+
+    funcx_functions = [
+        ssx_plot
+    ]
+
+
+class SSXPublish(GladierDefaults):
+
+    flow_definition = {
+      'Comment': 'Run Pilot and upload the result to search + petreldata',
+      'StartAt': 'SSXPilot',
+      'States': {
+        'SSXPilot': {
+          'Comment': 'Upload to petreldata, ingest to SSX search index',
+          'Type': 'Action',
+          'ActionUrl': 'https://api.funcx.org/automate',
+          'ActionScope': 'https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/automate2',
+          'ExceptionOnActionFailure': False,
+          'Parameters': {
+              'tasks': [{
+                'endpoint.$': '$.input.funcx_endpoint_non_compute',
+                'func.$': '$.input.ssx_pilot_funcx_id',
+                'payload.$': '$.input',
+            }]
+          },
+          'ResultPath': '$.SSXPilot',
+          'WaitTime': 600,
+          'End': True
+        }
+      }
+    }
+
+    flow_input = {}
+
+    required_input = []
+
+    funcx_functions = [
+        ssx_publish
+    ]
 # DialsVersion = GladierBaseTool()
 
 # Pilot = GladierBaseTool()
