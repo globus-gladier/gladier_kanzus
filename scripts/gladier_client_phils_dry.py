d3f82db196fba2cb80fddf481eab9c7297a5cd92
Rafael Vescovi <ravescovi@gmail.com>
2021-06-29 16:31:51 -0500

    debug1

--- scripts/gladier_client_phils_dry.py
+++ scripts/gladier_client_phils_dry.py
@@ -6,7 +6,7 @@ import numpy as np
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from gladier import GladierBaseClient
-import gladier.tests
+#import gladier.tests
 
 class KanzusTriggers:
     def __init__(self, folder_path):
@@ -38,73 +38,74 @@ class KanzusTriggers:
 class Handler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
+        print(event)
         if event.is_directory:
             return None
+
         elif event.event_type == 'created':
-            #event.src_path is the file watchdog found
-            # Event is created, you can process it now
             KanzusLogic(event.src_path)
+            return None
         elif event.event_type == 'modified':
-            #event.src_path is the file watchdog found
-            # Event is created, you can process it now
             KanzusLogic(event.src_path)
+            return None
+
+def KanzusLogic(new_file):
 
 
-def KanzusLogic(event_file):
-
-    new_file = os.path.basename(event_file)
-
-    cbf_pattern = r'(\w+_\d+_)(\d+).cbf'
-    cbf_parse = re.match(cbf_pattern, new_file)
-
-    ##check if extra_args
-    extra_folder = event_file.replace(local_dir,'')
-    extra_folder = extra_folder.replace(new_file,'')
-    extra_folder = extra_folder.replace('/','')
-
-    ##LOCAL processing dirs
-    loc_dir = os.path.join(base_input["input"]["base_local_dir"], extra_folder)
-    base_input["input"]["local_dir"] = loc_dir
-    base_input["input"]["local_proc_dir"] = loc_dir + '_proc'
-    base_input["input"]["local_upload_dir"] = loc_dir + '_images'
-
-    ##REMOTE processing dirs
-    data_dir = os.path.join(base_input["input"]["base_data_dir"], extra_folder)
-    base_input["input"]["data_dir"] = data_dir
-    base_input["input"]["proc_dir"] = data_dir + '_proc'
-    base_input["input"]["upload_dir"] = data_dir + '_images' 
+    cbf_num_pattern = r'(\w+_\d+_)(\d+).cbf'
+    cbf_parse = re.match(cbf_num_pattern, new_file)
 
     if cbf_parse is not None:
+
         cbf_base =cbf_parse.group(1)
         cbf_num =int(cbf_parse.group(2))
 
-        n_batch_transfer = 64
-        n_batch_plot = 1024
+        rel_path = new_file.replace(local_dir,'')
+
+        names = rel_path.split('/')
+        print(names)
+
+        # ##LOCAL processing dirs
+        # loc_dir = os.path.join(base_input["input"]["base_local_dir"], extra_folder)
+        # base_input["input"]["local_dir"] = loc_dir
+        # base_input["input"]["local_proc_dir"] = loc_dir + '_proc'
+        # base_input["input"]["local_upload_dir"] = loc_dir + '_images'
+
+        # ##REMOTE processing dirs
+        # data_dir = os.path.join(base_input["input"]["base_data_dir"], extra_folder)
+        # base_input["input"]["data_dir"] = data_dir
+        # base_input["input"]["proc_dir"] = data_dir + '_proc'
+        # base_input["input"]["upload_dir"] = data_dir + '_images' 
+
+
+
+        # n_batch_transfer = 128
+        # n_batch_plot = 1024
         
-        range_delta = n_batch_transfer
+        # range_delta = n_batch_transfer
 
-        if cbf_num%n_batch_transfer==0:
-            subranges = create_ranges(cbf_num-n_batch_transfer, cbf_num, range_delta)
-            new_range = subranges[0]
-            print('Transfer+Stills Flow')
-            base_input["input"]["input_files"]=f"{cbf_base}{new_range}.cbf"
-            base_input["input"]["input_range"]=new_range[1:-1]
-            base_input["input"]["trigger_name"]= os.path.join(
-                base_input["input"]["data_dir"], new_file
-            )
-            flow_input = base_input
-            #print("  Range : " + base_input["input"]["input_range"])
-            #print(flow_input)
-            flow = kanzus_client.run_flow(flow_input=flow_input)
-            print("  Trigger : " + base_input["input"]["trigger_name"])
-            print("  Range : " + base_input["input"]["input_range"])
-            print("  UUID : " + flow['action_id'])
-            print('')
+        # if cbf_num%n_batch_transfer==0:
+        #     subranges = create_ranges(cbf_num-n_batch_transfer, cbf_num, range_delta)
+        #     new_range = subranges[0]
+        #     print('Transfer+Stills Flow')
+        #     base_input["input"]["input_files"]=f"{cbf_base}{new_range}.cbf"
+        #     base_input["input"]["input_range"]=new_range[1:-1]
+        #     base_input["input"]["trigger_name"]= os.path.join(
+        #         base_input["input"]["data_dir"], new_file
+        #     )
+        #     flow_input = base_input
+        #     #print("  Range : " + base_input["input"]["input_range"])
+        #     #print(flow_input)
+        #     flow = kanzus_client.run_flow(flow_input=flow_input)
+        #     print("  Trigger : " + base_input["input"]["trigger_name"])
+        #     print("  Range : " + base_input["input"]["input_range"])
+        #     print("  UUID : " + flow['action_id'])
+        #     print('')
 
-        if cbf_num%n_batch_plot==0:
-            print('Plot Flow')
-            print("  UUID : " + workshop_flow['action_id'])
-            print('')
+        # if cbf_num%n_batch_plot==0:
+        #     print('Plot Flow')
+        #     print("  UUID : " + workshop_flow['action_id'])
+        #     print('')
 
 from gladier_kanzus.flows.phils_flow import flow_definition
 class KanzusSSXGladier(GladierBaseClient):
