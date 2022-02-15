87fdf8d46793a0f1c64eafdfee986b74f17442ab
Rafael Vescovi <ravescovi@anl.gov>
2022-02-15 09:46:01 -0600

    test morning

--- gladier_kanzus/tools/wait_trigger.py
+++ gladier_kanzus/tools/wait_trigger.py
@@ -2,15 +2,16 @@ from gladier import GladierBaseTool, generate_flow_definition
 
 def wait_trigger_file(**data):
     import os
-
+    import time
     data_dir = data['data_dir']
-    trigger_name = data["input"]["trigger_name"]
-    trigger_file = os.path.join(data_dir,os.path.basename(trigger_name))
+    filename = data["filename"]
+    
+    trigger_file = os.path.join(data_dir,filename)
 
     while not os.path.exists(trigger_file):
-        pass
-
-    return trigger_name
+        time.sleep(5)
+        
+    return trigger_file
 
 @generate_flow_definition(modifiers={
     'wait_trigger_file': {'endpoint': 'funcx_endpoint_non_compute'}
